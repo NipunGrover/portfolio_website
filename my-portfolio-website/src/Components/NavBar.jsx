c33a9ce089a10aaa5078c69bd966917eab2eb413
NipunGrover <ngrover5702@conestogac.on.ca>
2024-01-13 00:09:20 -0500

    added a premade css file/ will be adjusted later

--- my-portfolio-website/src/Components/NavBar.jsx
+++ my-portfolio-website/src/Components/NavBar.jsx
@@ -39,7 +39,7 @@ useEffect(() => {
       <Container>
       {/* Brand logo */}
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <img src={""} alt="Logo"/>
+            <img src={logo} alt="Logo"/>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
@@ -48,15 +48,15 @@ useEffect(() => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" style={{ marginRight: '1rem' }} className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
-            <Nav.Link href="#skills" style={{ marginRight: '1rem' }}className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}>Skills</Nav.Link>
-            <Nav.Link href="#projects" style={{ marginRight: '1rem' }}className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}>Projects</Nav.Link>
+            <Nav.Link href="#skills" style={{ marginRight: '1rem' }} className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}>Skills</Nav.Link>
+            <Nav.Link href="#projects" style={{ marginRight: '1rem' }} className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
           {/*this is where all the social media icons will be */}
             <div className="social-icon">
-              <a href="#"><img src={""}/></a>
-              <a href="#"><img src={""}/></a>
-              <a href="#"><img src={""}/></a>
+              <a href="#"><img src={navIcon1}/></a>
+              <a href="#"><img src={navIcon2}/></a>
+              <a href="#"><img src={navIcon3}/></a>
             </div>
             {/*console log is just to be replaced later on for actual navigation to the contact form on our application*/}
             <button className="vvd" onClick={() => console.log('connect')}/>
