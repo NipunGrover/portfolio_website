6856987ed0520691449d5a19d21f61421115e34a
NipunGrover <ngrover5702@conestogac.on.ca>
2024-01-12 22:39:48 -0500

    imported assets from assets folder, changed some navBar items, introduced a function to add and effect on scrolling

--- my-portfolio-website/src/Components/NavBar.jsx
+++ my-portfolio-website/src/Components/NavBar.jsx
@@ -1,33 +1,67 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+//import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useEffect, useState} from "react";
 
+import logo from '../assets/images/logo.svg';
+import navIcon1 from '../assets/images/nav-icon-1.svg';
+import navIcon2 from '../assets/images/nav-icon-2.svg';
+import navIcon3 from '../assets/images/nav-icon-3.svg';
+
+
+/*
+ * NavBar component - represents the navigation bar of the application.
+ */
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+useEffect(() => {
+  const onScroll = () => {
+    if(window.scrollY > 50) {
+      setScrolled(true);
+    }
+    else {
+      setScrolled(false);
+    }
+  }
+  window.addEventListener('scroll', onScroll);
+  return () => window.removeEventListener('scroll', onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (link) => {
+    setActiveLink(link);
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
+      {/* Brand logo */}
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <img src={""} alt="Logo"/>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
+
+         {/* Main navigation links */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" style={{ marginRight: '1rem' }} className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Home</Nav.Link>
+            <Nav.Link href="#skills" style={{ marginRight: '1rem' }}className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}>Skills</Nav.Link>
+            <Nav.Link href="#projects" style={{ marginRight: '1rem' }}className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}>Projects</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+          {/*this is where all the social media icons will be */}
+            <div className="social-icon">
+              <a href="#"><img src={""}/></a>
+              <a href="#"><img src={""}/></a>
+              <a href="#"><img src={""}/></a>
+            </div>
+            {/*console log is just to be replaced later on for actual navigation to the contact form on our application*/}
+            <button className="vvd" onClick={() => console.log('connect')}/>
+            <span>Let's Connect</span>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
