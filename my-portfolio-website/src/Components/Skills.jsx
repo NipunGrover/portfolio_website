9be4f20b7ef92dba0171274037e45eca9f9422ea
NipunGrover <ngrover5702@conestogac.on.ca>
2024-01-29 13:32:51 -0500

    added new logos for the skills section, aligned the text to create uniformity

--- my-portfolio-website/src/Components/Skills.jsx
+++ my-portfolio-website/src/Components/Skills.jsx
@@ -4,6 +4,17 @@ import { Container, Row, Col } from "react-bootstrap";
 import meter1 from "../assets/images/meter1.svg";
 import meter2 from "../assets/images/meter2.svg";
 import meter3 from "../assets/images/meter3.svg";
+import jsLogo from "../assets/images/js-logo.svg";
+import cssLogo from "../assets/images/css-logo.svg";
+import xamlLogo from "../assets/images/xaml.svg";
+import cshLogo from "../assets/images/csh-logo.svg";
+import reactLogo from "../assets/images/react-logo.svg";
+import cppLogo from "../assets/images/cpp-logo.svg";
+import cLogo from "../assets/images/c-logo.svg";
+import bootstrapLogo from "../assets/images/bootstrap-logo.svg";
+import aspLogo from "../assets/images/asp-net-logo.svg";
+import sqlLogo from "../assets/images/database-logo.svg";
+
 import colorSharp from "../assets/images/color-sharp.png";
 
 export const Skills = () => {
@@ -41,23 +52,48 @@ export const Skills = () => {
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.
                             </p>
                             <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            
+                                
                                 <div className="item">
-                                    <img src={meter1} alt="Image"/>
-                                    <h5>Web Development</h5>
+                                    <img src={jsLogo} alt="Image"/>
+                                    <h5>JavaScript</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="Image"/>
-                                    <h5>Brand Identity</h5>
+                                    <img src={cssLogo} alt="Image"/>
+                                    <h5>CSS</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="Image"/>
-                                    <h5>Logo Design</h5>
+                                    <img src={cshLogo} alt="Image"/>
+                                    <h5>C#</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter1} alt="Image"/>
-                                    <h5>Web Development</h5>
+                                    <img src={reactLogo} alt="Image"/>
+                                    <h5>React</h5>
                                 </div>
+                                <div className="item">
+                                    <img src={sqlLogo} alt="Image" className="white-logo" style={{ marginTop: '10px' }}/>
+                                    <h5 style={{ marginTop: '12px' }}>SQL</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={aspLogo} alt="Image" className="white-logo" style={{ marginTop: '50px' }}/>
+                                    <h5 style={{ marginTop: '20px' }}>.Net Framework and Core</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={bootstrapLogo} alt="Image" style={{ marginTop: '20px' }}/>
+                                    <h5 style={{ marginTop: '24px' }}>Bootstrap CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={cppLogo} alt="Image"/>
+                                    <h5>C++</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={cLogo} alt="Image"/>
+                                    <h5>C Development</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={xamlLogo} alt="Image" className="white-logo"/>
+                                    <h5>XAML (WPF)</h5>
+                                </div>
+                                
                             </Carousel>
                         </div>
                     </div>
